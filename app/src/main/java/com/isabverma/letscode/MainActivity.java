17b96765f0316cdfb717f613f7edff4e77daa086
Abhishek Verma <isabverma@gmail.com>
2017-02-09 05:40:32 +0530

    Added new intro activity

---
+++ app/src/main/java/com/isabverma/letscode/MainActivity.java
@@ -0,0 +1,163 @@
+package com.isabverma.letscode;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.google.firebase.crash.FirebaseCrash;
+import com.isabverma.letscode.intro.MainIntroActivity;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    Context context;
+    String folderPath = "file:android_asset/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.main_container);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.content_main, null);
+        final WebView webView = (WebView)layout.findViewById(R.id.content_main_web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(folderPath + "index.html");
+        getSupportActionBar().setTitle("GIT Basics");
+        mainLayout.removeAllViews();
+        mainLayout.addView(layout);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                String shareBody = "Let's Code, a very cool and informative android application available in play store to download for free.\nCheck it out now.\n\nDownload link : https://play.google.com/store/apps/details?id=com.isabverma.letscode";
+                String shareSub = "Let's Code - An Android Application";
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share using"));
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+    private Boolean exit = false;
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            if(exit){
+                finish();
+            }else{
+                Toast.makeText(this, "Press Back Again To Exit", Toast.LENGTH_SHORT).show();
+                exit = true;
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        exit = false;
+                    }
+                },2*1000);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(this, "Settings Selected..!!!", Toast.LENGTH_SHORT).show();
+        }else if(id == R.id.action_about_developers){
+            Toast.makeText(this, "About Developers Selected..!!!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this,MainIntroActivity.class);
+            startActivity(intent);
+        }else if(id == R.id.action_feedback){
+            Toast.makeText(this, "Feedback Selected..!!!", Toast.LENGTH_SHORT).show();
+        }else if(id == R.id.action_rate_us){ //for rate us
+            Uri uri = Uri.parse("market://details?id=com.isabverma.letscode.git");
+            Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+            // To count with Play market backstack, After pressing back button,
+            // to taken back to our application, we need to add following flags to intent.
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+            try {
+                startActivity(goToMarket);
+            } catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + context.getPackageName()))); }
+        }else if(id == R.id.action_share){
+            Toast.makeText(this, "Share Selected..!!!", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.main_container);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.content_main, null);
+        WebView webView = (WebView)layout.findViewById(R.id.content_main_web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        getSupportActionBar().setTitle(item.getTitle());
+        webView.loadUrl(folderPath + item.getTitleCondensed() + ".html");
+        mainLayout.removeAllViews();
+        mainLayout.addView(layout);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
